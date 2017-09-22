f4a86fda9f4173e4435293dd8d5f80726d7b0925
zhuangzebo <zebozhuang@163.com>
2017-09-22 23:21:36 +0800

    epoll使用mmap内存映射技术，省去多次拷贝文件描述符

--- src/ae_epoll.c
+++ src/ae_epoll.c
@@ -113,12 +113,18 @@ static int aeApiPoll(aeEventLoop *eventLoop, struct timeval *tvp) {
     aeApiState *state = eventLoop->apidata;
     int retval, numevents = 0;
 
+    /*
+        state->events使用了内存映射技术mmap, 不需要从内核将文件描述符取出，而是直接映射
+        select返回就绪的文件描述符个数
+        http://blog.csdn.net/xiajun07061225/article/details/9250579
+    */
     retval = epoll_wait(state->epfd,state->events,eventLoop->setsize,
             tvp ? (tvp->tv_sec*1000 + tvp->tv_usec/1000) : -1);
     if (retval > 0) {
         int j;
 
         numevents = retval;
+        /* 遍历已经激活的文件 */
         for (j = 0; j < numevents; j++) {
             int mask = 0;
             struct epoll_event *e = state->events+j;
