5c2bea6eb5cacbd9972134b341eee0bed90363ae
zhuangzebo <zebozhuang@163.com>
2017-09-24 15:40:23 +0800

    字符串

--- src/zipmap.c
+++ src/zipmap.c
@@ -1,3 +1,4 @@
+/* 字符串和字符串的Map映射 */
 /* String -> String Map data structure optimized for size.
  * This file implements a data structure mapping strings to other strings
  * implementing an O(n) lookup data structure designed to be very memory
