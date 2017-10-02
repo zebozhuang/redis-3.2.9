5e66cc844f2a25db9f35101a112eb0c5c719665d
zhuangzebo <zebozhuang@163.com>
2017-10-02 12:04:35 +0800

    地理位置

--- src/geo.h
+++ src/geo.h
@@ -5,14 +5,16 @@
 
 /* Structures used inside geo.c in order to represent points and array of
  * points on the earth. */
+/* 地理数据库实现 */
 typedef struct geoPoint {
-    double longitude;
-    double latitude;
-    double dist;
-    double score;
-    char *member;
+    double longitude;   /* 经度 */
+    double latitude;    /* 纬度 */
+    double dist;        /* ？*/
+    double score;       /* ? */
+    char *member;       /* ? */
 } geoPoint;
 
+/* 地理数组 */
 typedef struct geoArray {
     struct geoPoint *array;
     size_t buckets;
